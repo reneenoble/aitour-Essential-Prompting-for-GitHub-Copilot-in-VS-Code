b83da197bed5f28bec6c44d7ea38f03dd3337434
reneenoble <renee.noble@hotmail.com>
2024-12-10 12:44:55 +1100

    move python example

--- session-delivery-resources/01.2-multi-file-edit/app.py
+++ session-delivery-resources/01.2-multi-file-edit/app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/hello')
+def hello():
+    return "Hello, World!"
+
 @app.route('/')
 def index():
     return render_template('party_form.html')
